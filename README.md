63abf51f32663eb2e8577af43de7e3e3583704e4
upc-ghy <15155096254@163.com>
2023-12-04 17:16:19 +0800

    Upload README.md and modify test.py

--- README.md
+++ README.md
@@ -5,3 +5,21 @@ pytorch, python3+, CUDA programming
 Tested with cuda 11.7, python 3.7.16, pytorch 1.13.1, gcc 11.2.0
 
 # installation
+
+~~~shell
+# clone the tool project
+git clone https://github.com/upc-ghy/knn.git
+# cd knn
+cd knn/
+# install
+sudo python setup.py install
+~~~
+
+# A Simple Use Case
+
+~~~shell
+python test.py
+~~~
+
+
+
--- test.py
+++ test.py
@@ -8,24 +8,23 @@ knn = _C.knn
 class TestKNearestNeighbor(unittest.TestCase):
 
     def test_forward(self):
-        # D, N, M = 3, 100, 500
+        # use CPU
         ref = Variable(torch.rand(1, 3, 300))
         query = Variable(torch.rand(1, 3, 300))
         ref = ref.float()
         query = query.float()
 
         inds = torch.empty(query.shape[0], 1, query.shape[2]).long()
-
+        
+        print("CPU")
         knn(ref, query, inds)
-
 
         print(query)
         print(ref)
-
-        print("CPU")
         print(inds.shape)
         print(inds)
-
+        
+        # use GPU
         print("CUDA")
         ref = ref.cuda()
         query = query.cuda()
